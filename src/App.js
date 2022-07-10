f1912ea16a90f64c2b79fe356424f2c4394d84db
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-07-10 15:29:22 -0300

    style: styles global added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react'
-import Netflix from './pages/netflix';
+import React from 'react';
 
-function App(){
-    return(
+import Netflix from './pages/netflix';
+import GlobalStyle from './globalStyles';
+
+function App() {
+    return (
         <>
-           <Netflix />
+            <GlobalStyle />
+            <Netflix />
         </>
     )
 }
